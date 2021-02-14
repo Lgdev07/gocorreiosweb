89a8586f97813fd4d69bc9cc797edc60b48fee0a
Luan Gomes <luan_saopaulo06@hotmail.com>
2021-02-14 18:49:42 -0300

    add support to pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
   env: {
     REACT_APP_API_URL: process.env.REACT_APP_API_URL,
   },
-}
+  future: { webpack5: true }
+})
